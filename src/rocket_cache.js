390643b17582aed53b66258ce1e136d14e25cce0
li.jun <514332157@qq.com>
2015-08-13 21:09:48 +0800

    rename
    Change-Id: Ifc8f487bd0c2368f47babbba57b88d9cf2b72fc0

--- src/rocket_cache.js
+++ src/rocket_cache.js
@@ -49,26 +49,69 @@ global._RocketCache = function (opts) {
         conf.redis.clients.broadcast = client2;
     }
     if (typeof _DirectSolid === 'undefined') {
-        console.error('require module direct_solid not exist!');
+        _Log.error('require module direct_solid not exist!');
     }
     RocketCache.prototype.init = function (conf) {
-        this.sql = conf.sql;
-        this.type = conf.type;
-        this.columns = conf.columns;
-        this.dbPoolName = conf.dbPoolName;
-        this.dbCallBack = conf.dbCallBack;
-        if (typeof conf.type === 'undefined') {
+        this.sql = conf.sql;//mem和redis都没有时，使用sql语句从数据库读取
+        this.piece = conf.piece;//数据碎片化(根据id单条查询)
+        this.columns = conf.columns;//sql列(对应语句的?)
+        this.dbPoolName = conf.dbPoolName;//数据库名
+        this.fresh_time = conf.fresh_time;//数据刷新的时间
+        this.dbCallBack = conf.dbCallBack;//数据库读取callBack
+        this.type = 'RocketCache:' + conf.type;//数据类型
+        if (!conf.piece && typeof conf.type === 'undefined') {
             _Log.error('conf.type 为空');
         }
+        if (!isNaN(this.fresh_time)) {
+            this.key_set = [];
+        }
         this.stash = Stash.createStash(redis.createClient, conf);
+        // 定时刷新
+        if (this.key_set) {
+            var scope = this;
+            setInterval(function () {
+                scope.key_set.forEach(function (e) {
+                    scope.stash.del(e, function (err) {
+                        if (err) {
+                            _Log.errorObj('timer stash.del error:', err);
+                        }
+                    });
+                });
+                scope.key_set = [];
+            }, this.fresh_time);
+        }
     };
 
-    RocketCache.prototype.get = function (callBack) {
+    RocketCache.prototype.getType = function (callBack) {
         var scope = this;
         var fetch = function (done) {
             _DirectSolid(scope.sql, scope.dbCallBack, {
                 dbPoolName: scope.dbPoolName,
                 columns: scope.columns,
+                done: done
+            });
+        };
+        var cb = function (err, results) {
+            var backResult = results;
+            if (err) {
+                backResult = null;
+                _Log.errorObj('RocketCache get:', err);
+            }
+            callBack(backResult);
+        };
+        var data_key = scope.type;
+        this.key_set.push(data_key);
+        this.stash.get(data_key, fetch, cb);
+    };
+    RocketCache.prototype.getPiece = function (opts, key, callBack) {
+        if (this.piece !== true) {
+            return callBack(null);
+        }
+        var scope = this;
+        var fetch = function (done) {
+            _DirectSolid(opts.sql, scope.dbCallBack, {
+                dbPoolName: opts.dbPoolName,
+                columns: [key],
                 done: done
             });
         };
@@ -81,7 +124,25 @@ global._RocketCache = function (opts) {
             }
             callBack(backResult);
         };
-        this.stash.get('RocketCache:' + scope.type, fetch, cb);
+        var data_key = 'RocketCache:' + opts.type + ':' + key;
+        this.key_set.push(data_key);
+        this.stash.get(data_key, fetch, cb);
+    };
+    RocketCache.prototype.clearType = function (callBack) {
+        this.stash.del(this.type, function (err) {
+            if (err) {
+                _Log.errorObj('stash.del error:', err);
+            }
+            callBack();
+        });
+    };
+    RocketCache.prototype.clearPiece = function (type, key, callBack) {
+        this.stash.del('RocketCache:' + type + ':' + key, function (err) {
+            if (err) {
+                _Log.errorObj('stash.del error:', err);
+            }
+            callBack();
+        });
     };
     return new RocketCache(opts);
 };
